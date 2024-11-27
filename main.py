846a9fda350f25db0e42086dde7952b95befa5d6
Zavyalova Varvara <zvs1110@gmail.com>
2024-11-28 00:28:48 +0300

    add function is_git_repository

--- main.py
+++ main.py
@@ -7,3 +7,12 @@ def read_config(config_path):
     config.read(config_path)
     return config
 
+
+def is_git_repository(repo_path):
+    command = f"git -C {repo_path} rev-parse --is-inside-work-tree"
+    try:
+        result = subprocess.run(command, shell=True, capture_output=True, text=True, check=True)
+        return result.stdout.strip() == "true"
+    except subprocess.CalledProcessError:
+        return False
+
